9e0dece3691532df44fddf1969cc2d71aa682d04
Haru Iida <haru.ii.a9i1e@gmail.com>
2022-04-22 13:45:57 +0000

    checkout redmine

--- .gitpod/setup.sh
+++ .gitpod/setup.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 cd `dirname $0`
-cd ../..
+GITPODDIR=`pwd`
+cd ..
+BASEDIR=`pwd`
+cd ..
 git clone https://github.com/redmine/redmine.git
+cd redmine
+REDMINEDIR=`pwd`
+cd $REDMINEDIR/plugins
+cp -pr $BASEDIR .
+cp $GITPODDIR/database.yml $REDMINEDIR/config/
+cd $REDMINEDIR
+bundle install
+bundle exec rake db:migrarte
+bundle exec rake redmine:plugins:migrate
