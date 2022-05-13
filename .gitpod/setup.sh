44d047479087e881533562d67cee4a8c5380b742
Haru Iida <haru.ii.a9i1e@gmail.com>
2022-05-13 23:13:28 +0000

    Dockefile

--- .gitpod/setup.sh
+++ .gitpod/setup.sh
@@ -9,16 +9,15 @@ BASEDIR=$(pwd)
 PLUGIN_NAME=$(basename "$BASEDIR")
 cd ..
 REDMINEDIR=$(pwd)/redmine
-if [ ! -d redmine ]; then
-    git clone https://github.com/redmine/redmine.git -b "$REDMINE_VERSION"
-    cd redmine || exit
-    cd "$REDMINEDIR"/plugins || exit
-    ln -s "$BASEDIR" .
-    cp "$GITPODDIR"/database.yml "$REDMINEDIR"/config/
-    mkdir "$REDMINEDIR"/.vscode
-    cd "$REDMINEDIR"/.vscode || exit
-    ln -s "$GITPODDIR"/launch.json .
-fi
+
+cd redmine || exit
+cd "$REDMINEDIR"/plugins || exit
+ln -s "$BASEDIR" .
+#cp "$GITPODDIR"/database.yml "$REDMINEDIR"/config/
+mkdir "$REDMINEDIR"/.vscode
+cd "$REDMINEDIR"/.vscode || exit
+ln -s "$GITPODDIR"/launch.json .
+
 cd "$REDMINEDIR" || exit
 sed "s/__PLUGIN__NAME/$PLUGIN_NAME/" "$GITPODDIR"/gitpod.code-workspace > "$REDMINEDIR"/gitpod.code-workspace
 gem install ruby-debug-ide
